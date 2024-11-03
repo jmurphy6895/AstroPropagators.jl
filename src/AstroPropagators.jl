b6b06a94a5a04509274059def33d265210473396
Jordan Murphy <jmurphy6895@gmail.com>
2024-11-02 20:15:20 -0600

    feat: first iteration of impulsive maneuvers

--- src/AstroPropagators.jl
+++ src/AstroPropagators.jl
@@ -6,13 +6,16 @@ using ComponentArrays
 using LinearAlgebra
 using OrdinaryDiffEqAdamsBashforthMoulton
 using OrdinaryDiffEqCore
+using SciMLBase
 using StaticArraysCore
 
 ############################################################################################
 #                                         Includes
 ############################################################################################
+# Utilities
 include("auxiliary/util.jl")
 
+# Propagators
 include("propagators/Cowell.jl")
 #include("propagators/EDROMO.jl")
 include("propagators/GaussVE.jl")
@@ -20,6 +23,9 @@ include("propagators/GaussVE.jl")
 include("propagators/Milankovich.jl")
 #include("propagators/Stiefel-Scheifel.jl")
 include("propagators/USM.jl")
+
+# Events
+include("events/impulsive_maneuvers.jl")
 
 include("api.jl")
 
